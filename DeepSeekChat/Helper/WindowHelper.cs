820788b83cc124beef9faae5694c12f130f6e4bf
秋风Elipese <1175971310@qq.com>
2025-09-23 20:59:14 +0800

    upd & add: Add SetupPage

---
+++ DeepSeekChat/Helper/WindowHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeepSeekChat.Helper;
+
+public static class WindowHelper
+{
+    public static Task WaitForWindowCloseAsync(Window window)
+    {
+        var tcs = new TaskCompletionSource<object?>();
+
+        // 监听窗口关闭事件
+        window.Closed += (sender, args) =>
+        {
+            tcs.TrySetResult(null);
+        };
+
+        window.Activate(); // 激活窗口
+        return tcs.Task;
+    }
+}
---
+++ DeepSeekChat/Service/ModelPresetService.cs
@@ -0,0 +1,13 @@
+﻿using DeepSeekChat.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeepSeekChat.Service;
+
+public class ModelPresetService : JsonSeriailizingServiceBase<List<ModelPreset>>
+{
+
+}
